2e90b5d59216fc96968eb47284ea162797027a2f
mohamedAlaa <mohamedalhakar5@gmail.com>
2024-08-07 15:49:42 +0300

    solve error2  ElecOffice page

--- app/_components/ui/ActionCard.tsx
+++ app/_components/ui/ActionCard.tsx
@@ -11,7 +11,7 @@ function ActionCard({title,icon}:RequestProps) {
     <div className="w-[278px] h-15 rounded-xl bg-[#FFFFFF]  
  shadow-action  flex items-center justify-between   p-4  cursor-pointer    ">
      <div className='text-[14px] font-[600] leading-[26.24px] flex items-center gap-2 '>
-      <Image  src={icon} alt={icon} className='w-5 h-5 text-[#DDB762]'/>
+      <Image  src={icon} alt={'icon'} className='w-5 h-5 text-[#DDB762]'/>
       <span className='text-[#00262F]'>{title}</span> 
       </div>
       <div>
--- app/_components/ui/RequestCard.tsx
+++ app/_components/ui/RequestCard.tsx
@@ -9,7 +9,7 @@ function RequestCard({icon,title,color}:RequestProps) {
     <div className={`relative  w-34  h-13 flex items-center gap-2 p-5  rounded-[10px]      bg-white my-6 shadow-requstCard  before:content-[''] before:block before:w-[6px] before:h-full 
     before:bg-[${color}]   before:rounded-r-[10px] before:absolute before:top-0 before:right-0`}>
       <div  className="w-[18px] h-[18px] ">
-      <Image src={icon} alt={icon} className="w-full h-full"   />
+      <Image src={icon} alt={'icon'} className="w-full h-full"   />
       </div>
       <span>{title}</span>
     </div>
