acb5612c6b038926f6ca8f22c425a3e9ad1785d8
mohamedAlaa <mohamedalhakar5@gmail.com>
2024-08-07 15:33:59 +0300

    solve  ElecOffice page

--- app/_components/ui/ThirdHead.tsx
+++ app/_components/ui/ThirdHead.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
 
-function ThirdHead({title,className} ) {
+function ThirdHead({title } ) {
   return (
     <div>
-        <h1 className={`text-[14px] text-[#A6A4A4] leading-[20px] font-[600] mb-3 ${className}`}>{title}</h1>
+        <h1 className={`text-[14px] text-[#A6A4A4] leading-[20px] font-[600] mb-3  `}>{title}</h1>
     </div>
   )
 }
