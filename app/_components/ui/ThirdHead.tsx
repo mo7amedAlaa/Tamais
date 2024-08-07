d7cdf68355c34526eaacd208e734af129eb79bdf
mohamedAlaa <mohamedalhakar5@gmail.com>
2024-08-07 15:58:16 +0300

    solve error2  ElecOffice page

--- app/_components/ui/ThirdHead.tsx
+++ app/_components/ui/ThirdHead.tsx
@@ -1,5 +1,7 @@
-
-function ThirdHead({title } ) {
+interface ThirdHeadProps{
+  title:string
+}
+function ThirdHead({title }:ThirdHeadProps ) {
   return (
     <div>
         <h1 className={`text-[14px] text-[#A6A4A4] leading-[20px] font-[600] mb-3  `}>{title}</h1>
