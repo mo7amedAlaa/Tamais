03c78b0ca0f786498261ce3ca06df75a07918d13
mohamedAlaa <mohamedalhakar5@gmail.com>
2024-08-07 15:54:04 +0300

    solve error2  ElecOffice page

--- app/_components/ui/SecondHead.tsx
+++ app/_components/ui/SecondHead.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-
-export default function SecondHead({title}) {
+interface SecondHeadProps{
+  title:string,
+}
+export default function SecondHead({title}:SecondHeadProps) {
   return (
     <div className=' mb-10 '>
       <h1 className='text-[#00262F] font-[700] text-[24px]  text-center   '>{title}</h1>
