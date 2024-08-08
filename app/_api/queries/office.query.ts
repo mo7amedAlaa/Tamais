06925d62e77cff0356afb7247660d6ce9baf0efb
mohamedAlaa <mohamedalhakar5@gmail.com>
2024-08-08 19:29:55 +0300

    complete  ElecOffice page

--- app/_api/queries/office.query.ts
+++ app/_api/queries/office.query.ts
@@ -1,6 +1,6 @@
 import { API_ENDPOINTS } from "@/app/_helpers/config/constants";
 import { defaultAPI } from "../axios";
-export const analytics = async (token) => {
+export const analytics = async () => {
     const { data, status } = await defaultAPI.get(API_ENDPOINTS.LAWYER_ANALYTICS);
     return { data, status };
 };
