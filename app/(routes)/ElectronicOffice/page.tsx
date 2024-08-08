078c44dd09b95023493636006be32368e5e45118
mohamedAlaa <mohamedalhakar5@gmail.com>
2024-08-08 19:27:56 +0300

    complete  ElecOffice page

--- app/(routes)/ElectronicOffice/page.tsx
+++ app/(routes)/ElectronicOffice/page.tsx
@@ -1,16 +1,11 @@
-
 import Actions from '@/app/_components/pages/ElectronicOffice/Actions';
 import Balance from '@/app/_components/pages/ElectronicOffice/Balance';
-import ConsultationCard from '@/app/_components/pages/ElectronicOffice/ConsultationCard';
-import Details from '@/app/_components/pages/ElectronicOffice/Details';
 import Requests from '@/app/_components/pages/ElectronicOffice/Requests';
-import StatusCircle from '@/app/_components/pages/ElectronicOffice/StatusCircle';
-import CardHead from '@/app/_components/ui/CardHead';
 import SecondHead from '@/app/_components/ui/SecondHead';
+import StutasAndEventColom from '@/app/_components/ui/StutasAndEventColom';
 import ThirdHead from '@/app/_components/ui/ThirdHead';
 import React from 'react';
 import style from './style.module.css';
-import StutasAndEventColom from '@/app/_components/ui/StutasAndEventColom';
 const Page: React.FC = () => {
   const values = [9, 34, 34];
   const colors = ["#E56262", "#DDB762", "#00262F"]
