a0ebfec4c8e2ad01f3ca8e27f09e68147647e20e
mohamedAlaa <mohamedalhakar5@gmail.com>
2024-08-08 19:34:31 +0300

    complete  ElecOffice page

--- app/_components/ui/StutasAndEventColom.tsx
+++ app/_components/ui/StutasAndEventColom.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React from 'react'
-import CardHead from './CardHead'
 import StatusCircle from '../pages/ElectronicOffice/StatusCircle'
 import styles from '../../(routes)/ElectronicOffice/style.module.css'
 import { useState } from "react";
