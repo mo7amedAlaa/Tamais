b877ade210516f47bcc27647cbeea2dedbc89bd6
mohamedAlaa <mohamedalhakar5@gmail.com>
2024-08-10 06:04:28 +0300

    complete  ElecOffice page

--- app/_components/pages/ElectronicOffice/MyClients.tsx
+++ app/_components/pages/ElectronicOffice/MyClients.tsx
@@ -3,20 +3,16 @@ import RequestCleint from '@/app/_components/pages/ElectronicOffice/RequestClein
 import SecondHead from '@/app/_components/ui/SecondHead';
 import avatar from '@/public/avatar1.png';
 function MyClients() {
-  
-  
   return (
     <div className='container mx-auto min-h-screen'>
     <SecondHead title={'عملائي'} />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-[25px] gap-y-4  justify-center py-3 md:py-6 ">
-        <RequestCleint cleintImage={avatar} cleintName={'معتز حسن المالكي'} cleintCountry={'المملكة العربية السعودية'} cleintLocation={'الرياض'}/>
-        <RequestCleint cleintImage={avatar} cleintName={'معتز حسن المالكي'} cleintCountry={'المملكة العربية السعودية'} cleintLocation={'الرياض'}/>
-        <RequestCleint cleintImage={avatar} cleintName={'معتز حسن المالكي'} cleintCountry={'المملكة العربية السعودية'} cleintLocation={'الرياض'}/>
-        <RequestCleint cleintImage={avatar} cleintName={'معتز حسن المالكي'} cleintCountry={'المملكة العربية السعودية'} cleintLocation={'الرياض'}/>
-        <RequestCleint cleintImage={avatar} cleintName={'معتز حسن المالكي'} cleintCountry={'المملكة العربية السعودية'} cleintLocation={'الرياض'}/>
-        <RequestCleint cleintImage={avatar} cleintName={'معتز حسن المالكي'} cleintCountry={'المملكة العربية السعودية'} cleintLocation={'الرياض'}/>
-        <RequestCleint cleintImage={avatar} cleintName={'معتز حسن المالكي'} cleintCountry={'المملكة العربية السعودية'} cleintLocation={'الرياض'}/>
-        <RequestCleint cleintImage={avatar} cleintName={'معتز حسن المالكي'} cleintCountry={'المملكة العربية السعودية'} cleintLocation={'الرياض'}/>
+        <RequestCleint cleintImage={`${avatar}`} cleintName={'معتز حسن المالكي'} cleintCountry={'المملكة العربية السعودية'} cleintLocation={'الرياض'}/>
+        <RequestCleint cleintImage={`${avatar}`} cleintName={'معتز حسن المالكي'} cleintCountry={'المملكة العربية السعودية'} cleintLocation={'الرياض'}/>
+        <RequestCleint cleintImage={`${avatar}`} cleintName={'معتز حسن المالكي'} cleintCountry={'المملكة العربية السعودية'} cleintLocation={'الرياض'}/>
+        <RequestCleint cleintImage={`${avatar}`} cleintName={'معتز حسن المالكي'} cleintCountry={'المملكة العربية السعودية'} cleintLocation={'الرياض'}/>
+        <RequestCleint cleintImage={`${avatar}`} cleintName={'معتز حسن المالكي'} cleintCountry={'المملكة العربية السعودية'} cleintLocation={'الرياض'}/>
+       
   
         </div>
     </div>
