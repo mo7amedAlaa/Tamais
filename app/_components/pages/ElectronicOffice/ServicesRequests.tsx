930a4147237a2d1fb2f9b88d291a2427e6439e1e
mohamedAlaa <mohamedalhakar5@gmail.com>
2024-08-10 06:12:38 +0300

    complete  ElecOffice page

--- app/_components/pages/ElectronicOffice/ServicesRequests.tsx
+++ app/_components/pages/ElectronicOffice/ServicesRequests.tsx
@@ -5,6 +5,7 @@ import ActiveTitleTab from '@/app/_components/ui/ActiveTitleTab';
 import SecondHead from '@/app/_components/ui/SecondHead';
 import avatar from '@/public/avatar1.png';
 import { useState } from 'react';
+type TabType='Customers'|'DigitalGuide';
 function ServicesRequests() {
     const [activeTab, setActiveTab] = useState<TabType>('Customers');
    const buttonTitles = {
@@ -29,32 +30,56 @@ function ServicesRequests() {
         importance="مهم جدا"
         price="350"
         senderName="عبدالله حسن المالكي"
-        senderImage={avatar}
+        senderImage={`${avatar}`}
       />
         <RequestConsultationCard
-        status="مكتملة"
+        status="غير مكتملة"
         title="استشارة أفراد مرئية"
         date="6/12/2024"
         time="12:30 ص"
         importance="مهم جدا"
         price="350"
         senderName="عبدالله حسن المالكي"
-        senderImage={avatar}
+        senderImage={`${avatar}`}
       />
         <RequestConsultationCard
-        status="مكتملة"
+        status="غير مكتملة"
         title="استشارة أفراد مرئية"
         date="6/12/2024"
         time="12:30 ص"
         importance="مهم جدا"
         price="350"
         senderName="عبدالله حسن المالكي"
-        senderImage={avatar}
+        senderImage={`${avatar}`}
       />
+        <RequestConsultationCard
+        status="غير مكتملة"
+        title="استشارة أفراد مرئية"
+        date="6/12/2024"
+        time="12:30 ص"
+        importance="مهم جدا"
+        price="350"
+        senderName="عبدالله حسن المالكي"
+        senderImage={`${avatar}`}
+      />
+        <RequestConsultationCard
+        status="غير مكتملة"
+        title="استشارة أفراد مرئية"
+        date="6/12/2024"
+        time="12:30 ص"
+        importance="مهم جدا"
+        price="350"
+        senderName="عبدالله حسن المالكي"
+        senderImage={`${avatar}`}
+      />
+        
        
       </div>: <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-[25px] gap-y-4  justify-center py-3 md:py-6 ">
-        <RequestCleint cleintImage={avatar} cleintName={'معتز حسن المالكي'} cleintCountry={'المملكة العربية السعودية'} cleintLocation={'الرياض'}/>
-        <RequestCleint cleintImage={avatar} cleintName={'معتز حسن المالكي'} cleintCountry={'المملكة العربية السعودية'} cleintLocation={'الرياض'}/>
+        <RequestCleint clientImage={`${avatar}`} clientName={'معتز حسن المالكي'} clientCountry={'المملكة العربية السعودية'} clientLocation={'الرياض'}/>
+        <RequestCleint clientImage={`${avatar}`} clientName={'معتز حسن المالكي'} clientCountry={'المملكة العربية السعودية'} clientLocation={'الرياض'}/>
+        <RequestCleint clientImage={`${avatar}`} clientName={'معتز حسن المالكي'} clientCountry={'المملكة العربية السعودية'} clientLocation={'الرياض'}/>
+        <RequestCleint clientImage={`${avatar}`} clientName={'معتز حسن المالكي'} clientCountry={'المملكة العربية السعودية'} clientLocation={'الرياض'}/>
+        
         
         </div>}
     </div>
